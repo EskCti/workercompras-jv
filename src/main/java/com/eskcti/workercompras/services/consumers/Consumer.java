fab3963a4ebf63aa9af468a541a01f9aeb91cec5
eskokado <eskokado@gmail.com>
2023-11-24 10:53:54 -0300

    feat: impl email service

--- src/main/java/com/eskcti/workercompras/services/consumers/Consumer.java
+++ src/main/java/com/eskcti/workercompras/services/consumers/Consumer.java
@@ -1,25 +1,31 @@
-package com.eskcti.workercompras.consumers;
+package com.eskcti.workercompras.services.consumers;
 
 import com.eskcti.workercompras.models.Order;
+import com.eskcti.workercompras.services.emails.EmailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class Consumer {
 
     private final ObjectMapper objectMapper;
+    private final EmailService emailService;
 
     @RabbitListener(queues = {"${queue.name}"})
     public void consumer(@Payload Message message) {
         try {
             Order order = objectMapper.readValue(message.getBody(), Order.class);
-            System.out.println("Mensagem recebida no WorkerCompras: " + order);
+            log.info("Pedido recebido: {}", order);
+//            System.out.println("Mensagem recebida no WorkerCompras: " + order);
+            emailService.notificarClient(order);
         } catch (Exception e) {
             System.err.println("Erro ao converter Message para objeto Order: " + e.getMessage());
         }
