0f5cf9733d4e81bdbca6535a58928b1d4702b03f
eskokado <eskokado@gmail.com>
2023-11-25 07:11:37 -0300

    fix: removed import not used int GmailSmtpSendEmailService

--- src/main/java/com/eskcti/workercompras/services/emails/GmailSmtpSendEmailService.java
+++ src/main/java/com/eskcti/workercompras/services/emails/GmailSmtpSendEmailService.java
@@ -1,12 +1,6 @@
 package com.eskcti.workercompras.services.emails;
 
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
+import com.eskcti.workercompras.services.exceptions.EmailException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
