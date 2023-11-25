6fd07b761d80422bceb7b5d1550e511b5638f1d0
eskokado <eskokado@gmail.com>
2023-11-25 07:08:07 -0300

    feat: create feign config log

---
+++ src/main/java/com/eskcti/workercompras/configs/FeignConfiguration.java
@@ -0,0 +1,15 @@
+package com.eskcti.workercompras.configs;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+
+}
