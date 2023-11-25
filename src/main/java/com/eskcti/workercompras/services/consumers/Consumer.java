4c6317da4c8d7095f447cb1009fb90d548cf726e
eskokado <eskokado@gmail.com>
2023-11-25 07:16:42 -0300

    feat: add buscarcep by cepService into Consumer

--- src/main/java/com/eskcti/workercompras/services/consumers/Consumer.java
+++ src/main/java/com/eskcti/workercompras/services/consumers/Consumer.java
@@ -1,7 +1,10 @@
 package com.eskcti.workercompras.services.consumers;
 
+import com.eskcti.workercompras.models.Endereco;
 import com.eskcti.workercompras.models.Order;
+import com.eskcti.workercompras.services.CepService;
 import com.eskcti.workercompras.services.emails.EmailService;
+import com.eskcti.workercompras.services.exceptions.EnderecoNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +21,18 @@ public class Consumer {
 
     private final ObjectMapper objectMapper;
     private final EmailService emailService;
+    private final CepService cepService;
 
     @RabbitListener(queues = {"${queue.name}"})
     public void consumer(@Payload Message message) {
         try {
             Order order = objectMapper.readValue(message.getBody(), Order.class);
             log.info("Pedido recebido: {}", order);
-//            System.out.println("Mensagem recebida no WorkerCompras: " + order);
+            Endereco endereco = cepService.buscarCep(order.getZipCode());
+            if (endereco.getLogradouro() == null) {
+                throw new EnderecoNotFoundException("Endereço não encontrado");
+            }
+            order.setEndereco(endereco);
             emailService.notificarClient(order);
         } catch (Exception e) {
             System.err.println("Erro ao converter Message para objeto Order: " + e.getMessage());
